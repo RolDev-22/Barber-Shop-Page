3992a65e5ff46e3b0d51c5b62d1c1fded9a64344
RolDev-22 <rolandomuagui1629@gmail.com>
2025-07-27 13:01:56 -0600

    Comentario de anotación para inicio de proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
-
+//Para iniciar el proyecto ejecutamos en terminal "npm run dev"
 createRoot(document.getElementById("root")).render(
   <Router>
     <StrictMode>
